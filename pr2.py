eadeb3b743607a972cfc98d195448319e47b94e9
d21ce176 <97421522+d21ce176@users.noreply.github.com>
2022-04-17 10:25:52 +0530

    Add files via upload

---
+++ pr2.py
@@ -0,0 +1,9 @@
+import statistics
+List = [10, 50, 80, 70, 49, 23, 11, 4]
+minimum = min(List)
+maximum = max(List)
+mean = statistics.mean(List)
+standard_deviation = statistics.pstdev(List)
+variance = statistics.variance(List)
+print(minimum, maximum, '%.2f'%mean, '%.2f'%standard_deviation, '%.2f'%variance)
+print("this program is prepared by om and id :d21ce176")  
